1f8b130e4120f6a32f50b67c22f1f93deaf24b57
Quentin Pla <quentin-pla@hotmail.fr>
2020-04-26 14:47:37 +0200

    Implémentation des classes liées à l'hotel

--- src/main/java/models/BillingService.java
+++ src/main/java/models/BillingService.java
@@ -1,4 +1,16 @@
 package models;
 
 public class BillingService {
+    /**
+     * Hôtel lié au service
+     */
+    private Hotel hotel;
+
+    /**
+     * Constructeur
+     * @param hotel hotel lié
+     */
+    public BillingService(Hotel hotel) {
+        this.hotel = hotel;
+    }
 }

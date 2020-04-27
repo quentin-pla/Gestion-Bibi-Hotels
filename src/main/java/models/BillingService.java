d4f28e299ae7c9d381a8ad13c6de7d4c951b337d
Quentin Pla <quentin-pla@hotmail.fr>
2020-04-27 19:38:33 +0200

    Implémentation des classes de type service

--- src/main/java/models/BillingService.java
+++ src/main/java/models/BillingService.java
@@ -1,6 +1,28 @@
 package models;
 
+import java.util.ArrayList;
+
 public class BillingService {
+    /**
+     * Réduction pour les groupes en pourcentages
+     */
+    private final int groupDiscount = 10;
+
+    /**
+     * Réduction pour les clients réguliers en pourcentages
+     */
+    private final int regularClientDiscount = 20;
+
+    /**
+     * Liste des factures en attente de paiement
+     */
+    private ArrayList<Bill> pending_bills;
+
+    /**
+     * Archives des factures
+     */
+    private ArrayList<Bill> archives;
+
     /**
      * Hôtel lié au service
      */
@@ -12,5 +34,67 @@ public class BillingService {
      */
     public BillingService(Hotel hotel) {
         this.hotel = hotel;
+    }
+
+    /**
+     * Créer une facture
+     * @param reservation reservation
+     */
+    public void createBill(Reservation reservation) {
+        //Récupération des occupations liées à la réservation
+        ArrayList<Occupation> occupations = ClientService.getInstance().getReservationOccupations(reservation.getID());
+        //Montant total de la facture
+        double total_amount = 0.0;
+        //Pour chaque occupation
+        for (Occupation occupation : occupations)
+            //Incrémentation du montant total
+            total_amount += getTotalBillAmount(occupation);
+        //Création d'une nouvelle facture
+        pending_bills.add(new Bill(reservation.getCLIENT_ID(), total_amount));
+    }
+
+    /**
+     * Archiver une facture
+     * @param bill facture
+     */
+    public void archiveBill(Bill bill) {
+        //Archivage de la facture
+        archives.add(bill);
+        //Suppression de la facture dans les factures en attente
+        pending_bills.remove(bill);
+    }
+
+    /**
+     * Récupérer le montant total de la facture
+     * @param occupation occupation
+     * @return montant total
+     */
+    public double getTotalBillAmount(Occupation occupation) {
+        //Montant total
+        double total_amount = 0.0;
+        //Récupération du type de la chambre
+        RoomType roomType = occupation.getRoom().getRoomType();
+        //Ajout du montant du type de chambre au montant total pour le nombre de nuits passé
+        total_amount += roomType.getPRICE() * occupation.getReservation().getDURATION();
+        //Ajout du montant des services facturés
+        for (Service service : occupation.getBilledServices())
+            total_amount += service.getPRICE();
+        //Réduction en fonction du nombre de personnes (isolé/groupe)
+        if (occupation.getReservation().getPEOPLE_COUNT() > 1) total_amount -= (total_amount * groupDiscount)/100;
+        //Réduction en fonction du status du client (régulier ou pas)
+        if (occupation.getReservation().getClient().getIS_REGULAR())
+            total_amount -= (total_amount * regularClientDiscount)/100;
+        //Retour du montant total
+        return total_amount;
+    }
+
+    //************* GETTERS & SETTERS ***************//
+
+    public ArrayList<Bill> getPending_bills() {
+        return pending_bills;
+    }
+
+    public ArrayList<Bill> getArchives() {
+        return archives;
     }
 }
--- src/main/java/models/ClientService.java
+++ src/main/java/models/ClientService.java
@@ -1,16 +1,186 @@
 package models;
 
+import database.DatabaseModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static database.DatabaseConnection.getAvailableRoomsQuery;
+import static database.DatabaseConnection.selectQuery;
+
 public class ClientService {
     /**
-     * Hôtel lié au service
+     * Liste des occupations
      */
-    private Hotel hotel;
+    private ArrayList<Occupation> occupations;
+
+    /**
+     * Archives des occupations
+     */
+    private ArrayList<Occupation> archives;
 
     /**
      * Constructeur
-     * @param hotel hotel lié
      */
-    public ClientService(Hotel hotel) {
-        this.hotel = hotel;
+    private ClientService() {
+        this.occupations = new ArrayList<>();
+    }
+
+    /**
+     * Instance unique
+     */
+    private static ClientService instance = null;
+
+    /**
+     * Récupérer l'instance unique
+     */
+    public static ClientService getInstance() {
+        //Si l'instance n'est pas initialisée
+        if (instance == null)
+            //Initialisation de l'instance
+            instance = new ClientService();
+        //Retour de l'instance
+        return instance;
+    }
+
+    /**
+     * Récupérer l'historique des occupations d'un client
+     * @param client_id id du client
+     * @return occupations
+     */
+    public ArrayList<Occupation> getClientHistory(int client_id) {
+        //Historique
+        ArrayList<Occupation> history = new ArrayList<>();
+        //Pour chaque occupation dans les archives
+        for (Occupation archive : archives)
+            //Si l'archive est liée au client
+            if (archive.getReservation().getCLIENT_ID() == client_id)
+                //Ajout de l'archive dans l'historique
+                history.add(archive);
+        //Retour de l'historique
+        return history;
+    }
+
+    public ArrayList<Occupation> getReservationOccupations(int reservation_id) {
+        //Liste des occupations liées
+        ArrayList<Occupation> linkedOccupations = new ArrayList<>();
+        //Pour chaque occupation
+        for (Occupation occupation : occupations)
+            //Si l'id de réservation correspond
+            if (occupation.getRESERVATION_ID() == reservation_id)
+                //Ajout de l'occupation dans les résultats
+                linkedOccupations.add(occupation);
+        //Retour des résultats
+        return linkedOccupations;
+    }
+
+    /**
+     * Envoyer un mail au client
+     * @param mail adresse mail
+     */
+    public void sendAdvertising(String mail) {
+        System.out.println("Publicité envoyée à l'adresse mail " + mail);
+    }
+
+    /**
+     * Affecter des chambres pour une réservation
+     * @param reservation reservation
+     */
+    public boolean addOccupation(Reservation reservation) {
+        //Vérification s'il reste des chambres disponibles pour la réservation
+        List<Integer> results = getAvailableRoomsQuery(reservation);
+        //Si le nombre de chambres restantes est inférieur au nombre de chambre voulu
+        if (results.size() < reservation.getROOM_COUNT())
+            //Retourne faux
+            return false;
+        //Récupération du nombre de chambres nécessaire en fonction du nombre de chambre voulu
+        results = results.subList(0, reservation.getROOM_COUNT());
+        //Pour chaque chambre
+        for (Integer room_id : results)
+            //Ajout d'une nouvelle occupation dans la liste des occupations
+            occupations.add(new Occupation(reservation.getID(), room_id, false));
+        //Retourne vrai
+        return true;
+    }
+
+    /**
+     * Confirmer la présence du client lors de son arrivée
+     * @param reservation réservation
+     */
+    public void confirmClientPresence(Reservation reservation) {
+        //Pour chaque occupations
+        for (Occupation occupation : occupations) {
+            //Si l'occupation est liée à la réservation
+            if (occupation.getRESERVATION_ID() == reservation.getID()) {
+                //Passage du booléen concernant la présence du client à vrai
+                occupation.setIS_CLIENT_PRESENT("true");
+                //Mise à jour dans la base de données
+                occupation.updateColumn(Occupation.Columns.IS_CLIENT_PRESENT);
+            }
+        }
+    }
+
+    /**
+     * Mettre à jour la présence du client dans sa chambre
+     * @param occupation chambre occupée
+     * @param value présence du client
+     */
+    public void updatePresence(Occupation occupation, boolean value) {
+        //Passage du booléen à la valeur passée en paramètre
+        occupation.setIS_CLIENT_PRESENT(String.valueOf(value));
+        //Mise à jour dans la base de données
+        occupation.updateColumn(Occupation.Columns.IS_CLIENT_PRESENT);
+    }
+
+    /**
+     * Facturer un service pour un client occupant une chambre
+     * @param occupation chambre du client
+     * @param service service à facturer
+     */
+    public void billService(Occupation occupation, Service service) {
+        //Facturer le service au client occupant la chambre
+        occupation.billService(service);
+    }
+
+    /**
+     * Définir un client comme étant régulier
+     * @param client_id id du client
+     */
+    public void setRegularClient(int client_id) {
+        //Récupération du client depuis la base de données
+        Client client = (Client) selectQuery(DatabaseModel.Tables.CLIENTS, client_id);
+        //Passage du booléen à vrai
+        client.setIS_REGULAR("true");
+        //Mise à jour dans la base de données
+        client.updateColumn(Client.Columns.IS_REGULAR);
+    }
+
+    /**
+     * Archiver une occupation
+     * @param occupation occupation
+     */
+    public void archiveOccupation(Occupation occupation) {
+        //Archivage de l'occupation
+        archives.add(occupation);
+        //Suppression de l'occupation
+        occupations.remove(occupation);
+    }
+
+    //*************** GETTERS & SETTERS ***************//
+
+    public ArrayList<Occupation> getOccupations() {
+        return occupations;
+    }
+
+    public ArrayList<Occupation> getArchives() {
+        return archives;
+    }
+
+    public void setOccupations(ArrayList<Occupation> occupations) {
+        this.occupations = occupations;
+    }
+
+    public void setArchives(ArrayList<Occupation> archives) {
+        this.archives = archives;
     }
 }

8e2da0004076e7cd3f2c350c1a8dc7d728ead5b7
Quentin Pla <quentin-pla@hotmail.fr>
2020-04-28 19:41:07 +0200

    Revue méthode récupération des données de la base de données + ajout d'une méthode pour formatter un booléen reçu/à envoyer de la base de données

--- src/main/java/database/DatabaseModel.java
+++ src/main/java/database/DatabaseModel.java
@@ -1,12 +1,17 @@
 package database;
 
 import models.*;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import static database.DatabaseConnection.*;
+import static database.DatabaseConnection.insertQuery;
+import static database.DatabaseConnection.updateQuery;
 
 public abstract class DatabaseModel {
     /**
@@ -81,10 +86,29 @@ public abstract class DatabaseModel {
      * @param value valeur à attribuer
      */
     public void setColumnAttribute(String columnItem, String value) {
+        Class<?> parameterType = getColumnType(columnItem);
         try {
-            Method setColumn = getClass().getMethod("set" + columnItem, String.class);
-            setColumn.invoke(this, value);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            Method setColumn = getClass().getMethod("set" + columnItem, parameterType);
+            switch (parameterType.getSimpleName()) {
+                case "boolean":
+                    setColumn.invoke(this, Boolean.parseBoolean(parseBooleanFromString(value)));
+                    break;
+                case "int":
+                    setColumn.invoke(this, Integer.parseInt(value));
+                    break;
+                case "double":
+                    setColumn.invoke(this, Double.parseDouble(value));
+                    break;
+                case "String":
+                    setColumn.invoke(this, value);
+                    break;
+                case "Date":
+                    setColumn.invoke(this, new SimpleDateFormat("yyyy-MM-dd").parse(value));
+                    break;
+                default:
+                    break;
+            }
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | ParseException e) {
             e.printStackTrace();
         }
     }
@@ -93,7 +117,7 @@ public abstract class DatabaseModel {
      * Récupérer toutes la valeur de chaque colonne
      * @return valeur de chaque colonne
      */
-    public Map<String, String> getAttributesData() {
+    public Map<String,String> getAttributesData() {
         return getAttributesData(null);
     }
 
@@ -102,35 +126,30 @@ public abstract class DatabaseModel {
      * @param columnsFilter colonnes sélectionnées
      * @return valeur de chaque colonne
      */
-    public Map<String, String> getAttributesData(String columnsFilter) {
+    public Map<String,String> getAttributesData(String columnsFilter) {
         //Résultats
         Map<String,String> data = new HashMap<>();
         try {
-            //Pour chaque attributs de la classe
+            //Pour chaque colonne de la classe
             for (DatabaseColumns column : getColumns()) {
                 //Booléen pour trier les colonnes à récupérer
                 boolean isContained = (columnsFilter == null) || columnsFilter.contains(column.toString());
                 //Si la colonne est sélectionnée
                 if (isContained) {
+                    //Valeur de la colonne
+                    String value = "";
                     //Récupération du nom de la méthode
                     Method getColumn = getClass().getMethod("get" + column);
+                    //Si le type de retour est une date
+                    if (getColumn.getReturnType() == Date.class)
+                        //Formattage de la date
+                        value = new SimpleDateFormat("yyyy-MM-dd").format(getColumn.invoke(this));
                     //Appel de la méthode et récupération du résultat
-                    String value = getColumn.invoke(this).toString();
-                    //En fonction du résultat
-                    switch (value) {
-                        //Booléen à faux
-                        case "false":
-                            //Valeur vaut 0
-                            value = "0";
-                            break;
-                        //Booléen à vrai
-                        case "true":
-                            //Valeur vaut 1
-                            value = "1";
-                            break;
-                        default:
-                            break;
-                    }
+                    else value = getColumn.invoke(this).toString();
+                    //Si la colonne est un booléen
+                    if (getColumn.getAnnotatedReturnType().toString().equals("boolean"))
+                        //Conversion du booléen en entier
+                        value = parseBooleanFromString(value);
                     //Ajout de la colonne et de la valeur dans la map
                     data.put(column.toString(), value);
                 }
@@ -141,9 +160,40 @@ public abstract class DatabaseModel {
         return data;
     }
 
+    public Class<?> getColumnType(String column) {
+        try {
+            return getClass().getMethod("get" + column).getReturnType();
+        } catch (NoSuchMethodException e) { e.printStackTrace(); }
+        return null;
+    }
+
+    private String parseBooleanFromString(String string) {
+        //En fonction du string
+        switch (string) {
+            //Booléen à faux
+            case "false":
+                //Valeur vaut 0
+                return "0";
+            //Booléen à vrai
+            case "true":
+                //Valeur vaut 1
+                return "1";
+            //Entier à 0
+            case "0":
+                //Valeur vaut faux
+                return "false";
+            //Entier à 1
+            case "1":
+                //Valeur vaut vrai
+                return "true";
+            default:
+                return null;
+        }
+    }
+
     //************* GETTERS & SETTERS ***************//
 
     public int getID() { return ID; }
-    public void setID(String ID) { this.ID = Integer.parseInt(ID); }
+    public void setID(int ID) { this.ID = ID; }
     public Tables getTable() { return this.table; }
 }

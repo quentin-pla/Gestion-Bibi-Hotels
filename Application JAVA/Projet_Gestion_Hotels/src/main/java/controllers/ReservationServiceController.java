4ef37f1458a1d035aba37fed1f5748eb78989d7b
Quentin Pla <quentin-pla@hotmail.fr>
2020-05-01 00:32:40 +0200

    Implémentation interface Service Réservation + confirmation arrivée client

--- Application JAVA/Projet_Gestion_Hotels/src/main/java/controllers/ReservationServiceController.java
+++ Application JAVA/Projet_Gestion_Hotels/src/main/java/controllers/ReservationServiceController.java
@@ -1,13 +1,23 @@
 package controllers;
 
-import models.Hotel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import models.*;
+import views.ConfirmArrivalPanel;
 import views.ReservationServicePanel;
+
+import java.util.ArrayList;
 
 public class ReservationServiceController {
     /**
      * Fenêtre du service réservation
      */
     private ReservationServicePanel panel;
+
+    /**
+     * Fenêtre de confirmation de l'arrivée d'un client
+     */
+    private ConfirmArrivalPanel confirmArrivalPanel;
 
     /**
      * Hotel lié
@@ -19,23 +29,119 @@ public class ReservationServiceController {
      */
     public ReservationServiceController() {
         panel = new ReservationServicePanel();
-        //Initialisation des boutons
-        initPanel();
+        confirmArrivalPanel = new ConfirmArrivalPanel();
     }
 
     /**
      * Initialiser la fenêtre
      */
     private void initPanel() {
+        //Définition du titre de la fenêtre
+        panel.setPanelTitle("Service Réservation - " + hotel.getHOTEL_NAME());
         //Définition de l'action du bouton retour
         panel.getBack().setOnAction(e -> MainController.getInstance().switchToSelect());
+        //Affichage des boutons liés à une réservation sélectionnée
+        panel.getReservations().setOnMouseClicked(e -> refreshTableItems());
+        //Définition de l'action du bouton de confirmation de l'arrivée
+        panel.getArchive().setOnAction(e -> {
+            //Récupération de la réservation sélctionnée
+            Reservation reservation = panel.getReservations().getSelectionModel().getSelectedItem();
+            //Si la réservation existe
+            if (reservation != null)
+                //Archivage de la réservation
+                hotel.getReservationService().archiveReservation(reservation);
+            //Suppression du focus sur la réservation
+            panel.getReservations().getSelectionModel().clearSelection();
+            //Suppression de la réservation du tableau
+            panel.getReservations().getItems().remove(reservation);
+            //Rafraichissement
+            refreshTableItems();
+        });
+        //Définition de l'action du bouton de confirmation de l'arrivée du client
+        panel.getConfirmArrival().setOnAction(e -> {
+            //Récupération de la réservation sélctionnée
+            Reservation reservation = panel.getReservations().getSelectionModel().getSelectedItem();
+            //Si la réservation existe
+            if (reservation != null) {
+                //Initialisation de la fenêtre de confirmation d'arrivée
+                initConfirmArrivalPanel(reservation);
+                //Affichage de la fenêtre
+                MainController.getInstance().setWindow(confirmArrivalPanel);
+            }
+        });
+        //Définition de l'action du bouton de paiement des arrhes
+        panel.getMakePayment().setOnAction(e -> {
+            //Récupération de la réservation sélctionnée
+            Reservation reservation = panel.getReservations().getSelectionModel().getSelectedItem();
+            //Si la réservation existe
+            if (reservation != null)
+                //Confirmation du paiement des arrhes
+                hotel.getReservationService().confirmPayment(reservation);
+            //Rafraichissement
+            refreshTableItems();
+        });
+        //Liste des réservations
+        ObservableList<Reservation> items = FXCollections.observableArrayList(hotel.getReservationService().getReservations());
+        //Ajout des réservations à la table
+        panel.getReservations().setItems(items);
     }
 
-    /**
-     * //Définition du titre de la fenêtre
-     */
-    private void setPanelTitle() {
-        panel.setPanelTitle("Service Réservation - " + hotel.getHOTEL_NAME());
+    public void refreshTableItems() {
+        //Rafraichissement des éléments du tableau
+        panel.getReservations().refresh();
+        //Récupération de la réservation sélctionnée
+        Reservation reservation = panel.getReservations().getSelectionModel().getSelectedItem();
+        //Si la réservation existe
+        if (reservation != null) {
+            //Réservation non confirmée
+            boolean notConfirmed = !reservation.getIS_COMFIRMED() && reservation.getIS_PAYED();
+            //Réservation impayée
+            boolean notPayed = !reservation.getIS_PAYED();
+            //Réservation pas archivée
+            boolean notArchived = !reservation.getIS_ARCHIVED() && reservation.getIS_COMFIRMED() && reservation.getIS_PAYED();
+            //Définition visibilité bouton confirmation arrivée
+            panel.getConfirmArrival().setManaged(notConfirmed);
+            panel.getConfirmArrival().setVisible(notConfirmed);
+            //Définition visibilité bouton confirmation paiement
+            panel.getMakePayment().setManaged(notPayed);
+            panel.getMakePayment().setVisible(notPayed);
+            //Définition visibilité bouton archivage
+            panel.getArchive().setManaged(notArchived);
+            panel.getArchive().setVisible(notArchived);
+            //Affichage de la liste contenant les boutons
+            panel.getRefButtons().setVisible(true);
+        }
+        //Masquage de la liste contenant les boutons
+        else panel.getRefButtons().setVisible(false);
+
+    }
+
+    public void initConfirmArrivalPanel(Reservation reservation) {
+        //Définition du titre de la fenêtre
+        confirmArrivalPanel.setPanelTitle("Confirmation arrivée - Réservation n°" + reservation.getID());
+        //Définition de l'action du bouton retour
+        confirmArrivalPanel.getBack().setOnAction(e -> MainController.getInstance().setWindow(panel));
+        //Initialisation des champs de la fenêtre
+        confirmArrivalPanel.initFields(reservation.getPEOPLE_COUNT(),reservation.getClient().getFIRSTNAME(),reservation.getClient().getLASTNAME());
+        //Définition de l'action du bouton valider
+        confirmArrivalPanel.getValidate().setOnAction(e -> {
+            //Récupération des occupations liées à la réservation
+            ArrayList<Occupation> occupations = ClientService.getInstance().getReservationOccupations(reservation.getID());
+            //Pour chaque occupation
+            for (Occupation occupation : occupations)
+                //Pour chaque occupant
+                for (String[] occupantData : confirmArrivalPanel.retrieveFieldsData()) {
+                    //Ajout de l'occupant dans l'occupation si possible
+                    if (!occupation.addOccupant(new Occupant(occupation.getID(), occupantData[0], occupantData[1])))
+                        break;
+                }
+            //Confirmation de l'arrivée du client dans l'hotel
+            hotel.getReservationService().confirmArrival(reservation);
+            //Rafraichissement des éléments dans la table
+            refreshTableItems();
+            //Affichage de la liste des réservations
+            MainController.getInstance().setWindow(panel);
+        });
     }
 
     //************* GETTERS & SETTERS ***************//
@@ -48,6 +154,6 @@ public class ReservationServiceController {
 
     public void setHotel(Hotel hotel) {
         this.hotel = hotel;
-        setPanelTitle();
+        initPanel();
     }
 }
---
+++ Application JAVA/Projet_Gestion_Hotels/src/main/java/views/ConfirmArrivalPanel.java
@@ -0,0 +1,86 @@
+package views;
+
+import controllers.MainController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Page du service réservation
+ */
+public class ConfirmArrivalPanel extends BorderPane {
+    private Map<Integer,ObservableList<Control>> fieldsLinks = new HashMap<>();
+
+    private Button back = new Button("←");
+    private Label title = new Label();
+    private Button validate = new Button("Valider");
+    private VBox fields = new VBox();
+
+    public ConfirmArrivalPanel() {
+        setMinSize(MainController.width, MainController.height);
+        back.getStyleClass().add("back");
+        title.getStyleClass().add("h3");
+        BorderPane topContent = new BorderPane();
+        topContent.setLeft(back);
+        topContent.setCenter(title);
+        BorderPane.setAlignment(title, Pos.CENTER);
+        fields.setSpacing(20);
+        BorderPane.setMargin(fields, new Insets(10,40,0,40));
+        setTop(topContent);
+        setCenter(fields);
+    }
+
+    public void initFields(int people_count, String client_firstname, String client_lastname) {
+        fields.getChildren().clear();
+        for (int index = 1; index <= people_count; index++) {
+            HBox occupantData = new HBox();
+            occupantData.setAlignment(Pos.CENTER_LEFT);
+            occupantData.setSpacing(20);
+            Label peopleLabel = new Label("Personne n°" + index);
+            peopleLabel.setMinWidth(50);
+            TextField firstname = new TextField();
+            firstname.setPromptText("Nom");
+            firstname.setMinWidth(50);
+            if (index == 1) firstname.setText(client_firstname);
+            TextField lastname = new TextField();
+            lastname.setPromptText("Prénom");
+            lastname.setMinWidth(50);
+            if (index == 1) lastname.setText(client_lastname);
+            occupantData.getChildren().addAll(peopleLabel,firstname,lastname);
+            ObservableList<Control> links = FXCollections.observableArrayList();
+            links.addAll(firstname,lastname);
+            fieldsLinks.put(index,links);
+            fields.getChildren().add(occupantData);
+        }
+        fields.getChildren().add(validate);
+    }
+
+    public ArrayList<String[]> retrieveFieldsData() {
+        ArrayList<String[]> data = new ArrayList<>();
+        for (ObservableList<Control> links : fieldsLinks.values()) {
+            TextField firstname = (TextField) links.get(0);
+            TextField lastname = (TextField) links.get(1);
+            data.add(new String[]{firstname.getText(),lastname.getText()});
+        }
+        System.out.println(Arrays.toString(data.get(0)));
+        return data;
+    }
+
+    //************* GETTERS & SETTERS ***************//
+
+    public Button getBack() { return back; }
+
+    public Button getValidate() { return validate; }
+
+    public void setPanelTitle(String value) { title.setText(value); }
+}
